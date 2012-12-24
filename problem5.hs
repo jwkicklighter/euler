f87a0ec92ace8120bfea3094c6aeff0b1fd2c993
Jordan Kicklighter <jwkicklighter@yahoo.com>
2012-12-23 23:39:15 -0500

    Problem 5 has working concept but does not meet 60 second completion requirement.

--- problem5.hs
+++ problem5.hs
@@ -1,2 +1,17 @@
-solution :: Int
-solution = head [x| x <- [1..], maximum (map (mod x) [1..20]) == 0]
+-- Find the smallest (+) number divisible by 1-20
+-- 
+-- Solution should work (valid for 10) but takes much too long
+-- 
+-- Function takes in the upper limit (e.g. '20' for the actual solution)
+-- "problem5 10" should return "2520"
+
+solution :: Int -> Int
+solution n = head [x | x <- myList, x`divCheck`n == True]
+	where
+		myList = [x | x <- [1..], x`mod`n == 0, x`mod`(n-1) == 0 ]
+
+divCheck :: Int -> Int -> Bool
+divCheck _ 0 = True
+divCheck x n = if x`mod`n /= 0
+	then False
+	else divCheck x (n-1)
